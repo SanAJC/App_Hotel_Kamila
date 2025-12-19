7869411170e7e9770892cd1e83d6c7044e6f5f8e
san ats <san@gmail.com>
2025-12-19 14:55:04 -0500

    Cambio de orden en las fechas de entrad y salida

--- habitaciones/models.py
+++ habitaciones/models.py
@@ -47,13 +47,13 @@ class Venta_Habitacion(models.Model):
         ('transferencia', 'transferencia'),
     )
     huesped = models.ForeignKey(Huesped, on_delete=models.SET_NULL, null=True)
-    habitacion = models.ForeignKey(Habitacion, on_delete=models.SET_NULL, null=True)
-    fecha_entrada = models.DateTimeField(auto_now_add=True)  
-    fecha_salida = models.DateTimeField(null=True, blank=True)  
+    habitacion = models.ForeignKey(Habitacion, on_delete=models.SET_NULL, null=True) 
     precio_pagado = models.DecimalField(max_digits=10, decimal_places=0, null=True , blank=True)
     metodo_pago = models.CharField(max_length=20, choices=METODO_PAGO, default='efectivo')
     tipo_estadia = models.CharField(max_length=20, choices=TIPO_ESTADIA, default='estadia')
     extras = models.DecimalField(max_digits=10, decimal_places=0, null=True , blank=True)
+    fecha_entrada = models.DateTimeField(auto_now_add=True)  
+    fecha_salida = models.DateTimeField(null=True, blank=True) 
 
     def __str__(self):
         return f"{self.huesped} - {self.habitacion} - {self.fecha_entrada} - {self.fecha_salida} - {self.precio_pagado} - {self.metodo_pago}"
